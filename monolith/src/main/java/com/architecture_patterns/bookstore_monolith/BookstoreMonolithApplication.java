a78ee52b32f12036edbe0a855e6e963debf252f8
gadigenithin <gadigenithinyadav8683@gmail.com>
2025-05-17 19:26:16 +0530

    Intial Commit.

---
+++ monolith/src/main/java/com/architecture_patterns/bookstore_monolith/BookstoreMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.architecture_patterns.bookstore_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreMonolithApplication.class, args);
+	}
+
+}
